12c0862a52c8bddd5dda480b5c095049f78d9266
gauth555 <gautham.manghnani@outlook.com>
2015-10-09 11:08:15 -0500

    Map Reduce Code
    Contains various Map Reduce code like Word Count, Word Size and Patents.

---
+++ WordCount.java
@@ -0,0 +1,131 @@
+package in.cloudera.mapreduce;
+
+/*
+ * All org.apache.hadoop packaged can be imported using the jar files
+ */
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.fs.Path;
+
+/**
+ * @author Goutham!
+ * @version 1.0
+ * @since 09-Oct-2015
+ * @package in.cloudera.mapreduce
+ * <p>The Wordcount program counts the number of times words have appeared in the input
+ * file. We write a map reduce code to achieve this, where mapper makes key value pair from the input 
+ * file and reducer does aggregation on this key value pair.
+ */
+
+
+public class WordCount {
+	
+	/** 
+	 * @author Goutham!
+	 * @interface Mapper
+	 * <p>Map class is static and extends MapReduceBase and implements Mapper 
+	 * interface having four hadoop generics type LongWritable, Text, Text,
+	 * IntWritable
+	 */
+	
+	public static class Map extends Mapper<LongWritable,Text,Text,IntWritable>{
+
+		public void map(LongWritable key, Text value,
+				Context context)
+				throws IOException,InterruptedException {
+			
+			String line = value.toString();
+			StringTokenizer tokenizer = new StringTokenizer(line);
+
+			while (tokenizer.hasMoreTokens()) {
+				value.set(tokenizer.nextToken());
+				context.write(value, new IntWritable(1));
+			}
+	
+			
+		}
+		
+	}
+	public static class Reduce extends Reducer<Text,IntWritable,Text,IntWritable>{
+		
+		/**
+    	 * @method reduce
+    	 * <p>This method takes the input as key and list of values pair from mapper, it does aggregation
+    	 * based on keys and produces the final output.                                               
+    	 * @method_arguments key, values, output, reporter	
+    	 * @return void
+    	 */	
+    	
+		 /*
+		 * (non-Javadoc)
+		 * @see org.apache.hadoop.mapred.Reducer#reduce(java.lang.Object, java.util.Iterator, org.apache.hadoop.mapred.OutputCollector, org.apache.hadoop.mapred.Reporter)
+		 */
+
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context)
+				throws IOException,InterruptedException {
+			int sum=0;
+			// TODO Auto-generated method stub
+			for(IntWritable x: values)
+			{
+				sum+=x.get();
+			}
+			context.write(key, new IntWritable(sum));
+			
+		}
+		
+	}
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		//JobConf conf = new JobConf(WordCount.class);
+		Configuration conf= new Configuration();
+		
+		//conf.setJobName("mywc");
+		Job job = new Job(conf,"mywc");
+		
+		job.setJarByClass(WordCount.class);
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
+		
+		//conf.setMapperClass(Map.class);
+		//conf.setReducerClass(Reduce.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		
+		
+
+		Path outputPath = new Path(args[1]);
+			
+	        //Configuring the input/output path from the filesystem into the job
+	        
+	    FileInputFormat.addInputPath(job, new Path(args[0]));
+	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+			
+			//deleting the output path automatically from hdfs so that we don't have delete it explicitly
+			
+		outputPath.getFileSystem(conf).delete(outputPath);
+			
+			//exiting the job only if the flag value becomes false
+			
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
